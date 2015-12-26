0037713fc0dc624328d57f5a93833d63c71afdd5
David Mann <mannd@epstudiossoftware.com>
2015-12-25 18:34:57 -0700

    Initial Commit

---
+++ EP CalipersTests/EP_CalipersTests.swift
@@ -0,0 +1,36 @@
+//
+//  EP_CalipersTests.swift
+//  EP CalipersTests
+//
+//  Created by David Mann on 12/25/15.
+//  Copyright © 2015 EP Studios. All rights reserved.
+//
+
+import XCTest
+@testable import EP_Calipers
+
+class EP_CalipersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
