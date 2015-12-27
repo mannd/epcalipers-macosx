541a0f43e21fe23f50b592f5082050d40dd27de4
David Mann <mannd@epstudiossoftware.com>
2015-12-27 13:29:15 -0700

    Add MainWindowController, .gitignore file

---
+++ EP Calipers/MainWindowController.swift
@@ -0,0 +1,17 @@
+//
+//  MainWindowController.swift
+//  EP Calipers
+//
+//  Created by David Mann on 12/27/15.
+//  Copyright © 2015 EP Studios. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    override var windowNibName: String? {
+        return "MainWindowController"
+    }
+    
+}
