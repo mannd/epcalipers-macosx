e4dfdc89d8a752e43bb508b0b3df6db7f2b24ebb
David Mann <mannd@epstudiossoftware.com>
2015-12-29 20:41:51 -0700

    Add base image, open image functionality

--- EP Calipers/MainWindowController.swift
+++ EP Calipers/MainWindowController.swift
@@ -8,11 +8,43 @@
 
 import Cocoa
 import Quartz
+import AppKit
 
 class MainWindowController: NSWindowController {
     
+    @IBOutlet weak var imageView: IKImageView!
     override var windowNibName: String? {
         return "MainWindowController"
+    }
+    
+    override func awakeFromNib() {
+        let path = NSBundle.mainBundle().pathForResource("Normal 12_Lead ECG", ofType: "jpg")
+        let url = NSURL.fileURLWithPath(path!)
+        
+        imageView.setImageWithURL(url)
+        imageView.doubleClickOpensImageEditPanel = true
+        imageView.currentToolMode = IKToolModeMove
+        imageView.zoomImageToFit(self)
+    }
+    
+    @IBAction func openImage (sender: AnyObject) {
+        /* Present open panel. */
+        let extensions = "jpg/jpeg/JPG/JPEG/png/PNG/tiff/tif/TIFF/TIF"
+        let types = extensions.componentsSeparatedByString("/")
+        
+        /* Let the user choose an output file, then start the process of writing samples. */
+        let openPanel = NSOpenPanel()
+        openPanel.allowedFileTypes = types
+        openPanel.canSelectHiddenExtension = true
+        openPanel.beginSheetModalForWindow(self.window!,
+            completionHandler: {
+                (result: NSInteger) -> Void in
+                if result == NSFileHandlingPanelOKButton { // User did select an image.
+                    self.imageView.setImageWithURL(openPanel.URL)
+                }
+            }
+        )
+        
     }
     
 }
